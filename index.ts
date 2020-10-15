b00305ff8a5280ca274f476aecfba380100fc466
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-10-15 10:57:25 +0700

    socket.io

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello World</h1>');
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
 
 http.listen(3000, () => {
