e07b30a23fe0d2c2d97c2001e925826715d7d594
Abimanyu Gatya Dhaneswara <manyuabim9@gmail.com>
2020-10-15 13:21:37 +0700

    socket.io

--- index.ts
+++ index.ts
@@ -8,10 +8,13 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
+    // console.log('a user connected');
+    // socket.on('disconnect', () => {
+    //     console.log('user disconnected');
+    // });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg )
+    })
 });
 
 http.listen(3000, () => {
